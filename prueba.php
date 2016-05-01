6b5ff1c48f2279135dd9f0cecfc03142827802fd
Nathalia Bedoya <nathalia.bedoya@correounivalle.edu.co>
2016-05-01 21:14:18 +0000

    prueba de git con c9

---
+++ prueba.php
@@ -0,0 +1,227 @@
+
+
+<html>
+	<head>
+		<title>PROYECTO</title>
+		<style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#logo{
+			margin:auto;
+			width:auto;
+			}
+			
+			
+			#header {
+				margin:auto;
+				width:1580px;
+				font-family:Arial, Helvetica, sans-serif;
+			}
+			
+			ul, ol {
+	list-style: none;
+	color: #000000;
+	font-family: Constantia, "Lucida Bright", "DejaVu Serif", Georgia, serif;
+	font-style: normal;
+	font-variant: normal;
+			}
+			
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:#1A2246;
+				color:#fff;
+				text-decoration:none;
+				padding:10px 90px;
+				display:block;
+			}
+			
+			.nav li a:hover {
+				background-color:#424343;
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:440px;
+				top:0px;
+			}
+			
+		</style>
+   
+    <script>var __adobewebfontsappname__="dreamweaver"</script>
+    <script src="http://use.edgefonts.net/architects-daughter:n4:default.js" type="text/javascript"></script>
+
+	</head>
+
+	<body>
+
+
+<div class="logo" id="logo"><IMG SRC="logcli.jpg"> 	USUARIO:    </div>
+<form name="login-form" class="login-form" action="index_2.php" method="post">
+ <div class="footer">
+		
+        <input name="submit" type="submit" class="register" formaction="index_2.php" value="Cerrar Sesiòn" />
+        </div>
+        </form>
+
+
+<div id="header">
+  <ul class="nav">
+				<li><a href="index_2.php">Inicio</a></li>
+				<li><a href="">Servicios</a>
+					<ul>
+						<li><a href="">opcion</a></li>
+						<li><a href="">opcion</a></li>
+						<li><a href="">opcion</a></li>
+						<li><a href="">opcion</a>
+						
+						</li>
+					</ul>
+				</li>
+				<li><a href="">Acerca de</a>
+					<ul>
+						<li><a href="">opcion</a></li>
+						<li><a href="">opcion</a></li>
+						<li><a href="">opcion</a></li>
+						<li><a href="">opcion</a></li>
+					</ul>
+<li><a href="inicioclini.php">Inventario</a>
+					
+    </li>
+			  <li><a href="">Contacto</a></li>
+              
+              
+              
+              <li><a href="">Cuentas</a>
+              <ul>
+              <li><a href="registrar.php">Registrar Usuario</a></li>
+						<li><a href="modificar1.php">Modificar Usuario</a></li>
+						<li><a href="eliminar1.php">Eliminar Usuario</a></li>
+					</ul>
+             
+  
+		</div>
+		<BR></BR>
+	
+		
+		<font face="TakaoPGothic"<center><CENTER><H1>CLINICA ORIENTE S.A</H1></CENTER>
+		<BR></BR>
+		<BR></BR>
+		<font face="TakaoPGothic"<center><CENTER><H3>MISIÓN</H3></CENTER>
+		<BR></BR>
+		<BR></BR>
+		<center>Prestar servicios integrados de salud de primer y segundo nivel de atencion mediante una red</CENTER>
+       <BR></BR><center> de servicios a los usuarios de la comunidad de Cali y el Valle del Cauca, orientada a la</center>
+       <BR></BR><center> excelencia, para lograr la seguridad además de la satisfacción de nuestros clientes</center>
+       <BR></BR><center> y usuarios con personal competente; soportados en el mejoramiento continuo de los procesos</center>
+       <BR></BR><center> la sostenibilidad y la responsabilidad social empresarial.</center>
+       <BR></BR>
+		<BR></BR>
+			<BR></BR>
+		<BR></BR>
+		<font face="TakaoPGothic"<center><CENTER><H3>MISIÓN</H3></CENTER>
+		<center>Prestar servicios integrados de salud de primer y segundo nivel de atencion mediante una red</CENTER>
+       <BR></BR><center> de servicios a los usuarios de la comunidad de Cali y el Valle del Cauca, orientada a la</center>
+       <BR></BR><center> excelencia, para lograr la seguridad además de la satisfacción de nuestros clientes</center>
+       <BR></BR><center> y usuarios con personal competente; soportados en el mejoramiento continuo de los procesos</center>
+       <BR></BR><center> la sostenibilidad y la responsabilidad social empresarial.</center>
+	
+</body>
+
+</html>
+
+
+
+<style type="text/css">
+
+INGRESAR
+#login-box {
+	width:333px;
+	height: 352px;
+	padding: 58px 76px 0 76px;
+	color: #ebebeb;
+	font: 12px Arial, Helvetica, sans-serif;
+	background: url(images/login-box-backg.png) no-repeat left top;
+}
+
+#login-box img {
+	border:none;
+}
+
+#login-box h2 {
+	padding:0;
+	margin:0;
+	color: #ebebeb;
+	font: bold 44px "Calibri", Arial;
+}
+
+
+#login-box-name {
+	float: left;
+	display:inline;
+	width:80px;
+	text-align: right;
+	padding: 14px 10px 0 0;
+	margin:0 0 7px 0;
+}
+
+#login-box-field {
+	float: left;
+	display:inline;
+	width:230px;
+	margin:0;
+	margin:0 0 7px 0;
+}
+
+
+.form-login  {
+	width: 205px;
+	padding: 10px 4px 6px 3px;
+	border: 1px solid #0d2c52;
+	background-color:#1e4f8a;
+	font-size: 16px;
+	color: #ebebeb;
+}
+
+
+.login-box-options  {
+	clear:both;
+	padding-left:87px;
+	font-size: 11px;
+}
+
+.login-box-options a {
+	color: #ebebeb;
+	font-size: 11px;
+}
+
+#inicio {
+	font-family: architects-daughter;
+	font-size: 12px;
+	font-style: normal;
+	font-weight: 400;
+	color: #002982;
+	position: absolute;
+	height: 10px;
+	width: 10px;
+}
+</style>
