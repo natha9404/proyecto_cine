13c2895451b209da3b22e9bc288b07e110950812
edison mamian <edison.mamian@correounivalle.edu.co>
2016-05-28 19:32:08 +0000

    primeras clases de ORM

---
+++ ORM.php
@@ -0,0 +1,67 @@
+<?PHP
+    class ORM {
+        private static $basedatos;
+        protected static $tabla;
+        
+        function __construct(){
+            self::getConnection();
+        }
+        
+        private static function getConnection(){
+            require_once('BaseDatos.php');
+            self::$basedatos = BaseDatos::getConexion(DB_PROVIDER, DB_HOST, DB_USER, DB_PASSWORD, DB_DB);
+            
+        }
+        
+        public static function encontrar($id){
+            $resultado = self::where('id', $id);
+            return $resultado[0];
+        }
+        
+        public static function where ($campo, $valor){
+            $obj = null;
+            self::getConnection();
+            $query = "SELECT * FROM" . stat::$tabla . " WHERE " . $campo . " = ?";
+            $resultados = self::$basedatos->ejecutar($query, null, array($valor));
+            
+            if($resultados){
+                $class = get_called_class();
+                for($i=0; $i<sizeof($resultados); $i++){
+                    $obj [] = new $class($resultados[$i]);
+                }
+            }
+            
+            return $obj;
+        }
+        
+        public function save() {
+            $values = get_object_vars($this);
+            $filtered = null;
+            foreach ($values as $key => $value) {
+                if ($value !== null && $value !== '' && strpos($key, 'obj_') === false && $key !== 'id') {
+                    if ($value === false) {
+                        $value = 0;
+                    }
+                    $filtered[$key] = $value;
+                }
+            }
+            $columns = array_keys($filtered);
+            if ($this->id) {
+                $columns = join(" = ?, ", $columns);
+                $columns.= ' = ?';
+                $query = "UPDATE " . static ::$table . " SET $columns WHERE id =" . $this->id;
+            } else {
+                $params = join(", ", array_fill(0, count($columns), "?"));
+                $columns = join(", ", $columns);
+                $query = "INSERT INTO " . static ::$table . " ($columns) VALUES ($params)";
+            }
+            $result = self::$database->execute($query, null, $filtered);
+            if ($result) {
+                $result = array('error' => false, 'message' => self::$database->getInsertedID());
+            } else {
+                $result = array('error' => true, 'message' => self::$database->getError());
+            }
+            return $result;
+        }
+    }
+?>
---
+++ Usuario.php
@@ -0,0 +1,12 @@
+<?PHP
+    class Usuario {
+        private $id;
+        private $usuario;
+        private $contrasena;
+        private $nombres;   
+        private $apellidos;
+        private $fecha_nacimiento;
+        private $email;
+    }
+
+?>
