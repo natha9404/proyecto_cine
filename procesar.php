aacbe1db77ba451acdf54e49811b4011e4c7a282
edison mamian <edison.mamian@correounivalle.edu.co>
2016-06-06 22:10:15 +0000

    eliminar cuenta

--- procesar.php
+++ procesar.php
@@ -3,6 +3,7 @@
 	require_once('tablas_active/usuario.php');
 
 	$opcion = $_POST['opcion'];
+	
 
 	if ($opcion == 1){ // Registrar un usuario en la aplicacion
 		// obtengo los datos por POST
@@ -37,19 +38,85 @@
 		//echo 'cualquier cosa';
 		//echo $UserName;
 		if($Passwod == $pass){
-			session_start();
-			$_SESSION['username'] = $UserName;
-	
-			header('Location: '.'Pagina_Pelicula_login.php');
+			if($UserName = 'admin'){
+				session_start();
+				$_SESSION['username'] = $UserName;
+		
+				header('Location: '.'Pagina_Admin.php');
+			}else{
+				session_start();
+				$_SESSION['username'] = $UserName;
+		
+				header('Location: '.'Pagina_Pelicula_login.php');
+			}
+			
+			
 		}else{
 			header('Location: '.'login_error.php');
 		}
 		
 		
-	}else if($opcion == 3){ // registrar pelicula
+	}else if($opcion == 3){ // modificar usuario
+		
+		$nombre = $_POST['nombre'];
+		$apellido = $_POST['apellido'];
+		$email = $_POST['email'];
+		$UserName = $_POST['UserName'];
+		$Passwod = $_POST['Passwod'];
+		$fecha = $_POST['fecha'];
+		
+		echo $usuario1;
+		echo $fecha1;
+		
+		$usuario = new Usuario();
+		$usuario->usuario = $UserName;
+		$usuario->nombres = $nombre;
+		$usuario->apellidos = $apellido;
+		$usuario->contrasena = $Passwod;
+		$usuario->email = $email;
+		$usuario->fecha_nacimiento = $fecha;
+		
+		echo $UserName;
+		
+		$nuevo = Usuario::find_by_usuario($UserName);
+		
+		$nuevo ->update_attributes(array('nombres'=>$nombre, 'apellidos'=>$apellido, 'email'=>$email, 'contrasena'=>$Passwod));
+		
+		//$nuevo->save();
+		session_start();
+		$_SESSION['username'] = $UserName;
 
+		header('Location: '.'Pagina_Pelicula_login.php');
+	}else if($opcion ==4) {
+		
+		
+		
+			session_start();
+			$hola = Usuario::find_by_usuario($_SESSION['username']);
+			echo 'hola';
+			echo  $_SESSION['username'];
+			$hola->delete();
+			header('Location: '.'logout.php');
+		
 	}
-
+	
+	
+	
+	
+	
+	
+	class procesar {
+		public function encontrar_usuario($usuario){
+		$hola = Usuario::find_by_usuario($usuario);
+		return $hola;
+		}
+		
+		public function listar(){
+			$lista = Usuario::all();
+			return $lista;
+		}
+	}
+	
 	
 	/*$post = Usuario::find_by_nombres('edison felipe');
 	$post -> contrasena = '123456';
